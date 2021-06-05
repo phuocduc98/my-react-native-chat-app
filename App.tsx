8c15b54023e918900bb7d2ac2178357ce56ba2b6
ducng <mr.stell92@gmail.com>
2021-06-05 17:36:37 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+import { default as theme } from './custom-theme.json';
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">HOME</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
